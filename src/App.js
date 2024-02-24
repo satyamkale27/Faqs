b79ee5d67093ddea9b4d6f61e6b4a27b26684eb6
satyam kale <satyamkale271@gmail.com>
2024-02-24 19:49:05 +0530

    added list no counting with array map index with i parameter

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ function Accordion({ data }) {
 function AccordionItem({ num, title, text }) {
   return (
     <div className="item">
-      <p className="number">{num}</p>
-      <p className="text">{title}</p>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
       <p className="icon">-</p>
       <div className="content-box">{text}</div>
     </div>
