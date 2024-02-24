c4567981b6f5aa0311180a05a554170931fd1e9d
satyam kale <satyamkale271@gmail.com>
2024-02-24 20:10:49 +0530

    acanged the prop key

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function Accordion({ data }) {
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={i} />
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
       ))}
     </div>
   );
